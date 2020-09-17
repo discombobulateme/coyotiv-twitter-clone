9f0ea60b6ecf26792fece47e1a90e69026b89d49
discombobulateme <p@discombobulate.me>
2020-09-17 18:28:39 +0200

    Add user model

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -5,8 +5,54 @@ const userSchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      unique: true,
+      required: true,
     },
+    handle: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    profilePicture: String,
+    location: String,
+    website: String,
+    bio: String,
+    joinedAt: {
+      type: Date,
+      default: new Date(),
+    },
+    following: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        autopopulate: { maxDepth: 1 },
+      },
+    ],
+    followers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        autopopulate: { maxDepth: 1 },
+      },
+    ],
+    tweets: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Tweet',
+        autopopulate: { maxDepth: 3 },
+      },
+    ],
+    likedTweets: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Tweet',
+        autopopulate: { maxDepth: 1 },
+      },
+    ],
   },
   { timestamps: true }
 )
