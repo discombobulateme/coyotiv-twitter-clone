92ae364dc8c6824df792945a53d98ffed099194e
discombobulateme <p@discombobulate.me>
2020-09-17 23:03:44 +0200

    Add vue config file

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -3,4 +3,16 @@ module.exports = {
     disableHostCheck: true,
   },
   lintOnSave: false,
+  chainWebpack: config => {
+    const svgRule = config.module.rule('svg')
+
+    svgRule.uses.clear()
+
+    svgRule
+      .use('vue-loader')
+      .loader('vue-loader-v16')
+      .end()
+      .use('vue-svg-loader')
+      .loader('vue-svg-loader')
+  },
 }
