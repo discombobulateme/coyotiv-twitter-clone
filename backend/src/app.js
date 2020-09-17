9afc21ca690403abf542e935e93657c5c284498c
discombobulateme <p@discombobulate.me>
2020-09-17 22:58:26 +0200

    Add tweets route

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,6 +6,7 @@ const logger = require('morgan')
 
 const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
+const tweetsRouter = require('./routes/tweets')
 
 require('./database-connection')
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
+app.use('/tweets', tweetsRouter)
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
